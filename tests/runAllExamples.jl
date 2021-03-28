4a9c60d8006b5fc1caad6fa3a9c92120133ca1cc
angus-lewis <angus.lewis@student.adelaide.edu.au>
2021-03-29 09:43:51 +1030

    changed dir

--- tests/runAllExamples.jl
+++ tests/runAllExamples.jl
@@ -1,8 +1,8 @@
-# include("../examples/paperNumerics/example4_2.jl"); 
-# include("../examples/paperNumerics/example4_3.jl"); 
-# include("../examples/paperNumerics/example4_4.jl"); 
-# include("../examples/paperNumerics/example4_5.jl"); 
-# include("../examples/paperNumerics/toymodel.jl"); 
-# include("../examples/whichR.jl"); 
+include("../examples/paperNumerics/example4_2.jl"); 
+include("../examples/paperNumerics/example4_3.jl"); 
+include("../examples/paperNumerics/example4_4.jl"); 
+include("../examples/paperNumerics/example4_5.jl"); 
+include("../examples/paperNumerics/toymodel.jl"); 
+include("../examples/whichR.jl"); 
 include("../examples/baseAdvComparison/example4_2.jl"); 
 include("../examples/baseAdvComparison/example4_5.jl")
