e3e8a203cf3645b5ee3c26b6583d3e8b3da4b016
angus-lewis <angus.lewis@student.adelaide.edu.au>
2021-06-09 11:35:11 +0930

    reinstate D CMEParams to order 49

--- src/CMEParamsData/readCMEParams.jl
+++ src/CMEParamsData/readCMEParams.jl
@@ -1,11 +1,17 @@
+using JSON, JLD2
+
 CMEParams = Dict()
 for i in 1:26
-    @load pwd()*"/dev/CMEParamsData/CMEParams"*string(i)*".jld2" tempDict
-    merge!(CMEParams,tempDict)
+    @load pwd()*"/src/CMEParamsData/CMEParams"*string(i)*".jld2" tempDict
+    for k in keys(tempDict)
+        if k < 50
+            CMEParams[k] = tempDict[k]
+        end
+    end
 end
 
-open("dev/CMEParams.json","w") do f
+open("src/CMEParamsData/CMEParams.json","w") do f
     JSON.print(f, CMEParams)
 end
 
-@save "dev/CMEParams.jld2" CMEParams
+@save "src/CMEParamsData/CMEParams.jld2" CMEParams
